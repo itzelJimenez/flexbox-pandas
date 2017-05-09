c8ef11c3f50b19b953c7872aa7d077364869f370
Itzel <itzeljimenez.do@gmail.com>
2017-05-09 16:39:05 +0000

    Funcionalidad de borrar y restaurar imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 //Imágenes
 var x=document.getElementsByClassName("x");
+var imagenes=document.getElementsByClassName("img");
 //botones
 var buttOrign=document.getElementById("orign");
 var buttExtcn=document.getElementById("extcn");
@@ -7,11 +8,21 @@ var buttRestr=document.getElementById("restr");
 //textos
 var origen=document.getElementById("origen");
 var extincion=document.getElementById("extincion");
-
+//contador
+var click=1;
 //Funcionalidad Etiquetas de cierre de imágenes
 for(var i=0;i<x.length;i++){
-	x[i].addEventListener("click", prueba)
+	x[i].addEventListener("click", eliminar)
 }
-function prueba(){
-	alert("hola");
+function eliminar(){
+	this.nextElementSibling.style.visibility="hidden";
+	this.style.display="none"
 }
+function restr(){
+	for(var i=0; i<imagenes.length;i++){
+		imagenes[i].style.visibility="initial";
+		imagenes[i].previousElementSibling.style.display="block";
+	}
+}
+
+buttRestr.addEventListener("click", restr)
